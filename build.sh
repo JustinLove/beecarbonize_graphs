f2376c63b2c00b2f8f349d25f8de6d977d08a1b9
Justin Love <git@JustinLove.name>
2023-06-28 16:33:55 -0500

    archive first version and prepare for updates

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ dot -Tsvg beecarbonize.dot | sed -e's/<svg/<svg id="graph"/' > beecarbonize.dot.
 cat header.html beecarbonize.dot.svg footer.html > public/beecarbonize_tech_tree.html
 dot -Tsvg beecarbonizeevents.dot | sed -e's/<svg/<svg id="graph"/' > beecarbonizeevents.dot.svg
 cat header.html beecarbonizeevents.dot.svg footer.html | sed -e's/Tech Tree/Event Chances/' > public/beecarbonize_event_chances.html
+cp public/*.html "public/2.0.1/"
